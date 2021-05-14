83f46f8679e7d87b32aec5bae11ec707f58ff8ba
Artem Melnykov <melnikov.artem.v@gmail.com>
2021-05-13 23:01:57 -0400

    Run NYSE with USD

--- run/trading.py
+++ run/trading.py
@@ -12,40 +12,25 @@ def run():
     cerebro = bt.Cerebro()
     store = bt.stores.IBStore(port=7497, clientId=1, host='127.0.0.1')
 
-    # portfolio = PortfoliosRepository().get_by_id(2)
-    # tickers = PortfoliosRepository.get_tickers(portfolio)
-    # for ticker in tickers:
-    #     data = store.getdata(
-    #         dataname=ticker.symbol,
-    #         sectype='STK',
-    #         exchange='SMART',
-    #         # currency='USD',
-    #         timeframe=bt.TimeFrame.Ticks,  # compression=5,
-    #         rtbar=True,
-    #         fromdate=datetime.strptime("2021-01-19T00:00:00", "%Y-%m-%d" + "T%H:%M:%S"),
-    #     )
-    #     cerebro.resampledata(data, name=ticker.symbol, timeframe=bt.TimeFrame.Seconds, compression=10)
-
-    data = store.getdata(
-        dataname="AAPL",
-        sectype='STK',
-        exchange='SMART', # NYSE
-        currency='USD',
-        timeframe=bt.TimeFrame.Ticks,
-        # compression=5,
-        rtbar=True,
-        # fromdate=datetime.strptime("2021-01-19T00:00:00", "%Y-%m-%d" + "T%H:%M:%S"),
-    )
-
-    cerebro.resampledata(data, timeframe=bt.TimeFrame.Seconds, compression=10)
+    portfolio = PortfoliosRepository().get_by_id(2)
+    tickers = PortfoliosRepository.get_tickers(portfolio)
+    for ticker in tickers:
+        data = store.getdata(
+            dataname=ticker.symbol,
+            sectype='STK',
+            exchange='SMART',
+            currency='USD',
+            timeframe=bt.TimeFrame.Ticks,  # compression=5,
+            rtbar=True,
+            # fromdate=datetime.strptime("2021-05-01T00:00:00", "%Y-%m-%d" + "T%H:%M:%S"),
+        )
+        cerebro.resampledata(data, name=ticker.symbol, timeframe=bt.TimeFrame.Seconds, compression=10)
 
     cerebro.broker = store.getbroker()
 
-    cerebro.addstrategy(MdMACDStrategy)
+    cerebro.addstrategy(MdMACDStrategy, portfolio_version_id=1)
     cerebro.addsizer(WeightedSizer)
     result = cerebro.run()
-    print(result)
-    # cerebro.plot()
 
 
 if __name__ == "__main__":
