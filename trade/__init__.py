6cea8bb544d03062a26aa452f9d73e36bef9ffa6
Artem Melnykov <melnikov.artem.v@gmail.com>
2021-06-13 13:19:36 -0400

    Package refactoring (#24)
    * Base for interactive CLI
    
    * Foundation for portfolio commands
    
    * Run linting

---
+++ trade/__init__.py
@@ -0,0 +1,15 @@
+"""
+Main entry point
+
+Author: Artem Melnykov
+Contributor: Ihor Mokriienko
+
+Usage:
+
+    trade
+    trade runner account
+
+
+"""
+__version__ = "0.1"
+__copyright__ = "2021"
---
+++ trade/commands/portfolios_commands.py
@@ -0,0 +1,47 @@
+from nubia import command, argument
+from rich.console import Console
+from rich.table import Table
+
+from trade.repositories import PortfoliosRepository
+
+
+@command
+def portfolios() -> None:
+    """
+    List existing portfolios
+    """
+    portfolios = PortfoliosRepository.list()
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("ID")
+    table.add_column("Name")
+    table.add_column("Amount")
+    for portfolio in portfolios:
+        table.add_row(f"{portfolio.id}", portfolio.name, f"{portfolio.amount}")
+
+    console.print(table)
+
+
+@command
+class Portfolio:
+    """
+    Portfolio managing
+    """
+
+    @command("import")
+    @argument("file", description="YAML file to import", positional=True)
+    def import_from_file(self, file: str):
+        """
+        Import from yml file
+        """
+        from rich.console import Console
+
+        console = Console()
+        console.log("Successfully imported")
+
+    @command
+    def versions(self):
+        """
+        Print available versions of portfolio
+        """
+        pass
