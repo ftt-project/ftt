82ab6300f2c78af10bab12b68f3265ae1e8456cf
Artem Melnykov <melnikov.artem.v@gmail.com>
2021-07-05 08:14:09 -0400

    Implemented PortfolioWeightsAllocationStep and PortfoliosStatsHandler

--- tests/handlers/test_portfolio_stats_handler.py
+++ tests/handlers/test_portfolio_stats_handler.py
@@ -6,4 +6,10 @@ from trade.handlers.portfolio_stats_handler import PortfoliosStatsHandler
 class TestPortfoliosStatsHandler:
     @pytest.fixture
     def subject(self):
-        return PortfoliosStatsHandler
+        return PortfoliosStatsHandler()
+
+    def test_runs(self, subject, portfolio_version, weight, security):
+        result = subject.handle(portfolio_version=portfolio_version)
+
+        assert result.is_ok()
+        assert "planned_weights" in result.value
--- trade/handlers/portfolio_stats_handler.py
+++ trade/handlers/portfolio_stats_handler.py
@@ -1,5 +1,14 @@
 from trade.handlers.handler.handler import Handler
+from trade.handlers.handler.retrun_result import ReturnResult
+from trade.handlers.portfolio_steps.portfolio_weights_allocation_step import PortfolioWeightsAllocationStep
 
 
 class PortfoliosStatsHandler(Handler):
-    pass
+    key = "portfolio_stats"
+
+    params = ["portfolio_version"]
+
+    handlers = [
+        (PortfolioWeightsAllocationStep, "portfolio_version"),
+        ReturnResult("stats")
+    ]
