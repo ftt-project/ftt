d64512557c2b522d961426cc9473c1c72fd8b870
Artem Melnykov <melnikov.artem.v@gmail.com>
2021-07-09 16:55:54 -0400

    Run example command taking configuration from default yml config file

--- trade/cli/commands/example_commands.py
+++ trade/cli/commands/example_commands.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import os
+import pathlib
 
 from nubia import command, context
 from rich.table import Table
@@ -6,6 +7,7 @@ from rich.table import Table
 from trade.handlers.portfolio_associate_securities_hanlder import (
     PortfolioAssociateSecuritiesHandler,
 )
+from trade.handlers.portfolio_config_handler import PortfolioConfigHandler
 from trade.handlers.portfolio_creation_handler import PortfolioCreationHandler
 from trade.handlers.portfolio_stats_handler import PortfoliosStatsHandler
 from trade.handlers.securities_loading_handler import SecuritiesLoadingHandler
@@ -28,9 +30,12 @@ def example():
     """
     ctx = context.get_context()
 
+    # TODO: take env from context
     Storage.initialize_database(application_name="ftt", environment="dev")
 
-    result = PortfolioCreationHandler().handle(name="S&P companies", amount=10000)
+    config = read_config()
+
+    result = PortfolioCreationHandler().handle(name=config.name, amount=config.budget)
     portfolio = result.value
 
     ctx.console.print("Portfolio successfully created", style="bold green")
@@ -46,36 +51,35 @@ def example():
     )
     ctx.console.print(table)
 
-    ctx.console.print("Loading securities information", style="bold green")
+    with ctx.console.status("[bold green]Loading securities information") as _:
+        for symbol in config.symbols:
+            ctx.console.print(f"- {symbol}")
 
-    for symbol in ["AAPL", "SHOP", "MSFT"]:
-        ctx.console.print(f"- {symbol}")
-    today = datetime.today()
-    result = SecuritiesLoadingHandler().handle(
-        securities=["AAPL", "SHOP", "MSFT"],
-        start_period=datetime(today.year, 1, 1),
-        end_period=datetime(today.year, today.month, today.day),
-        interval="1d",
-    )
-    _ = result.value
+        result = SecuritiesLoadingHandler().handle(
+            securities=config.symbols,
+            start_period=config.period_start,
+            end_period=config.period_end,
+            interval=config.interval,
+        )
+        _ = result.value
 
-    ctx.console.print(
-        "Portfolio successfully associated with securities", style="bold green"
-    )
-    _ = PortfolioAssociateSecuritiesHandler().handle(
-        securities=["AAPL", "SHOP", "MSFT"], portfolio_version=portfolio.versions[0]
-    )
+    with ctx.console.status(
+        "[bold green]Portfolio successfully associated with securities"
+    ) as _:
+        _ = PortfolioAssociateSecuritiesHandler().handle(
+            securities=config.symbols, portfolio_version=portfolio.versions[0]
+        )
 
-    ctx.console.print("Calculating weights", style="bold green")
-    result = WeightsCalculationHandler().handle(
-        portfolio=portfolio,
-        portfolio_version=portfolio.versions[0],
-        start_period=datetime(today.year, 1, 1),
-        end_period=datetime(today.year, today.month, today.day),
-        interval="1d",
-        persist=True,
-    )
-    _ = result.value
+    with ctx.console.status("[bold green]Calculating weights") as _:
+        result = WeightsCalculationHandler().handle(
+            portfolio=portfolio,
+            portfolio_version=portfolio.versions[0],
+            start_period=config.period_start,
+            end_period=config.period_end,
+            interval=config.interval,
+            persist=True,
+        )
+        _ = result.value
 
     result = PortfoliosStatsHandler().handle(portfolio_version=portfolio.versions[0])
     portfolio_stats = result.value
@@ -88,3 +92,11 @@ def example():
         )
     ctx.console.print(table)
     ctx.console.print("Weights are calculated and saved", style="bold green")
+
+
+def read_config():
+    realpath = pathlib.Path().resolve()
+    path = os.path.join(realpath, "config", "example_portfolio.yml")
+    result = PortfolioConfigHandler().handle(path=path)
+    # TODO: handle exception
+    return result.value
