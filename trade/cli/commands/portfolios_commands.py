8918e25a531ad3811a779e3adcdf8685487ef951
Artem Melnykov <melnikov.artem.v@gmail.com>
2021-07-19 09:30:45 -0400

    Portfolios import command

--- trade/cli/commands/portfolios_commands.py
+++ trade/cli/commands/portfolios_commands.py
@@ -6,9 +6,12 @@ from trade.cli.renderers.portfolio_versions.portfolio_versions_list import (
 )
 from trade.cli.renderers.portfolios.portfolio_details import PortfolioDetails
 from trade.cli.renderers.weights.weights_list import WeightsList
+from trade.handlers.portfolio_config_handler import PortfolioConfigHandler
+from trade.handlers.portfolio_creation_handler import PortfolioCreationHandler
 from trade.handlers.portfolio_load_handler import PortfolioLoadHandler
 from trade.handlers.portfolio_versions_list_handler import PortfolioVersionsListHandler
 from trade.handlers.portfolios_list_handler import PortfoliosListHandler
+from trade.handlers.securities_loading_handler import SecuritiesLoadingHandler
 from trade.handlers.weights_list_handler import WeightsListHandler
 
 
@@ -55,15 +58,39 @@ class PortfoliosCommands:
         ).render()
 
     @command("import")
-    @argument("file", description="YAML file to import", positional=True)
-    def import_from_file(self, file: str) -> None:
+    @argument("path", description="YAML file to import", positional=True)
+    def import_from_file(self, path: str) -> None:
         """
         Import from yml file
         """
-        from rich.console import Console
+        ctx = context.get_context()
+        config_result = PortfolioConfigHandler().handle(path=path)
+        result = PortfolioCreationHandler().handle(
+            name=config_result.value.name, amount=config_result.value.budget
+        )
+        if result.is_ok():
+            ctx.console.print("[bold green]Portfolio successfully created")
+        else:
+            ctx.console.print("[bold red]Failed to create portfolio:")
+            ctx.console.print(result.value)
+            return
 
-        console = Console()
-        console.log("Successfully imported")
+        with ctx.console.status("[bold green]Loading securities information") as _:
+            for symbol in config_result.value.symbols:
+                ctx.console.print(f"- {symbol}")
+
+            securities_result = SecuritiesLoadingHandler().handle(
+                securities=config_result.value.symbols,
+                start_period=config_result.value.period_start,
+                end_period=config_result.value.period_end,
+                interval=config_result.value.interval,
+            )
+            if securities_result.is_ok():
+                ctx.console.print(
+                    "[bold green]Securities information successfully imported"
+                )
+            else:
+                ctx.console.print("[bold red]Failed to import securities information")
 
     @command
     def versions(self) -> None:
