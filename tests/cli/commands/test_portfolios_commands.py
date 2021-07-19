01e3e3c9fe24929c48f0ae0fe790e89908de27cd
Artem Melnykov <melnikov.artem.v@gmail.com>
2021-07-19 09:30:45 -0400

    Portfolios import command

--- tests/cli/commands/test_portfolios_commands.py
+++ tests/cli/commands/test_portfolios_commands.py
@@ -1,6 +1,10 @@
+import os
+import pathlib
+
 import pytest
 
 from trade.cli.commands.portfolios_commands import PortfoliosCommands
+from trade.storage.models import Portfolio
 
 
 class TestPortfoliosCommands:
@@ -11,6 +15,12 @@ class TestPortfoliosCommands:
     @pytest.fixture(autouse=True)
     def mock_context(self, mocker, context):
         mocker.patch("trade.cli.commands.portfolios_commands.context", new=context)
+
+    @pytest.fixture
+    def path_to_config(self):
+        realpath = pathlib.Path().resolve()
+        path = os.path.join(realpath, "config", "example_portfolio.yml")
+        return path
 
     def test_list(self, subject, mocker, portfolio):
         mocked = mocker.patch('trade.cli.commands.portfolios_commands.PortfoliosList')
@@ -33,3 +43,20 @@ class TestPortfoliosCommands:
         subject.details(portfolio.id)
 
         assert portfolio_version in mocked.call_args[0][1]
+
+    def test_import_correct_config_from_file(self, subject, path_to_config):
+        before = Portfolio.select().count()
+        subject.import_from_file(path_to_config)
+        after = Portfolio.select().count()
+
+        assert (after - before) == 1
+
+    @pytest.mark.skip(reason="Not implemented test")
+    def test_fails_to_import_portfolio_and_assets_info_is_not_requested(self, subject):
+        pass
+
+    def test_on_correct_config_request_assets_info(self, subject, mocker, path_to_config):
+        mocked = mocker.patch("trade.cli.commands.portfolios_commands.SecuritiesLoadingHandler",
+                              **{"return_value.handle.return_value.value": True})
+        subject.import_from_file(path_to_config)
+        mocked.return_value.handle.assert_called_once()
--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -3,6 +3,13 @@ import pytest
 
 @pytest.fixture
 def context(mocker):
+    context = mocker.Mock()
+    context.__enter__ = mocker.Mock(return_value=(mocker.Mock(), None))
+    context.__exit__ = mocker.Mock(return_value=None)
+
+    console = mocker.Mock()
+    console.status.return_value = context
+
     m = mocker.Mock()
-    m.get_context.return_value.console
+    m.get_context.return_value.console = console
     return m
