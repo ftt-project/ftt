4f742d5c92b813b9d6150838b4f305a4108e6733
Artem Melnykov <melnykov.artem.v@gmail.com>
2022-02-02 19:56:57 -0500

    Update dependencies

--- tests/handlers/test_portfolio_version_load_active_handler.py
+++ tests/handlers/test_portfolio_version_load_active_handler.py
@@ -1,6 +1,8 @@
 import pytest
 
-from ftt.handlers.portfolio_version_load_active_handler import PortfolioVersionLoadActiveHandler
+from ftt.handlers.portfolio_version_load_active_handler import (
+    PortfolioVersionLoadActiveHandler,
+)
 
 
 class TestPortfolioVersionLoadActiveHandler:
@@ -8,8 +10,12 @@ class TestPortfolioVersionLoadActiveHandler:
     def subject(self):
         return PortfolioVersionLoadActiveHandler()
 
-    def test_process_returns_active_portfolio_version_by_portfolio_id(self, subject, portfolio_version_factory, portfolio):
-        active_portfolio_version = portfolio_version_factory(portfolio=portfolio, version=1)
+    def test_process_returns_active_portfolio_version_by_portfolio_id(
+        self, subject, portfolio_version_factory, portfolio
+    ):
+        active_portfolio_version = portfolio_version_factory(
+            portfolio=portfolio, version=1
+        )
         active_portfolio_version.active = True
         active_portfolio_version.save()
         _ = portfolio_version_factory(portfolio=portfolio, version=2)
