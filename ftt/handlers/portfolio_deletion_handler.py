2c11c4768a9d59183870ab93a44d7ae3b57179e0
IhorMok <igor4ik08@gmail.com>
2022-02-19 23:03:14 +0200

    Add TestPortfolioDeletionValidateHandler

--- ftt/handlers/portfolio_deletion_handler.py
+++ ftt/handlers/portfolio_deletion_handler.py
@@ -1,5 +1,8 @@
 from ftt.handlers.handler.handler import Handler
 from ftt.handlers.handler.return_result import ReturnResult
+from ftt.handlers.portfolio_steps.portfolio_deletion_validate_step import (
+    PortfolioDeletionValidateStep,
+)
 from ftt.handlers.portfolio_steps.portfolio_load_step import PortfolioLoadStep
 from ftt.handlers.portfolio_steps.portfolio_delete_step import PortfolioDeleteStep
 
@@ -9,6 +12,7 @@ class PortfolioDeletionHandler(Handler):
 
     handlers = [
         (PortfolioLoadStep, "portfolio_id"),
+        (PortfolioDeletionValidateStep, PortfolioLoadStep.key),
         (PortfolioDeleteStep, PortfolioLoadStep.key),
         (ReturnResult, PortfolioDeleteStep.key),
     ]
--- tests/handlers/test_portfolio_deletion_handler.py
+++ tests/handlers/test_portfolio_deletion_handler.py
@@ -1,6 +1,9 @@
 import pytest
 
 from ftt.handlers.portfolio_deletion_handler import PortfolioDeletionHandler
+from ftt.handlers.portfolio_steps.portfolio_deletion_validate_step import (
+    PortfolioDeletionValidateStep,
+)
 from tests.helpers import reload_record
 
 
@@ -15,3 +18,29 @@ class TestPortfolioDeletionHandler:
         assert result.is_ok()
         assert result.value == portfolio
         assert reload_record(portfolio).deleted_at is not None
+
+
+class TestPortfolioDeletionValidateHandler:
+    @pytest.fixture
+    def subject(self):
+        return PortfolioDeletionValidateStep
+
+    def test_process_errors_when_portfolio_version_is_activate(
+        self, subject, portfolio, portfolio_version
+    ):
+        portfolio_version.active = True
+        portfolio_version.save()
+
+        result = subject.process(portfolio=portfolio)
+
+        assert result.is_err()
+
+    def test_process_succeeds_when_portfolio_version_is_not_activate(
+        self, subject, portfolio, portfolio_version
+    ):
+        portfolio_version.active = False
+        portfolio_version.save()
+
+        result = subject.process(portfolio=portfolio)
+
+        assert result.is_ok()
