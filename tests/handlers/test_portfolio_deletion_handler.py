744cede0e1f871c42e38b30d8a813a667dc3acfd
Ihor <47272869+IhorMok@users.noreply.github.com>
2022-02-20 21:12:54 +0200

    Add TestPortfolioDeletionValidateHandler (#112)
    * Add TestPortfolioDeletionValidateHandler
    
    * Update TestPortfolioDeletionHandler

--- tests/handlers/test_portfolio_deletion_handler.py
+++ tests/handlers/test_portfolio_deletion_handler.py
@@ -15,3 +15,23 @@ class TestPortfolioDeletionHandler:
         assert result.is_ok()
         assert result.value == portfolio
         assert reload_record(portfolio).deleted_at is not None
+
+    def test_handle_portfolio_with_active_portfolio_version(
+        self, subject, portfolio, portfolio_version
+    ):
+        portfolio_version.active = True
+        portfolio_version.save()
+
+        result = subject.handle(portfolio_id=portfolio.id)
+
+        assert result.is_err()
+
+    def test_handle_portfolio_with_not_active_portfolio_version(
+        self, subject, portfolio, portfolio_version
+    ):
+        portfolio_version.active = False
+        portfolio_version.save()
+
+        result = subject.handle(portfolio_id=portfolio.id)
+
+        assert result.is_ok()
