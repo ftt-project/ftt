934490853ae4b9890483c83d297484b72e2e5dcf
IhorMok <igor4ik08@gmail.com>
2022-02-20 21:11:27 +0200

    Update TestPortfolioDeletionHandler

--- tests/handlers/test_portfolio_deletion_handler.py
+++ tests/handlers/test_portfolio_deletion_handler.py
@@ -1,9 +1,6 @@
 import pytest
 
 from ftt.handlers.portfolio_deletion_handler import PortfolioDeletionHandler
-from ftt.handlers.portfolio_steps.portfolio_deletion_validate_step import (
-    PortfolioDeletionValidateStep,
-)
 from tests.helpers import reload_record
 
 
@@ -19,28 +16,22 @@ class TestPortfolioDeletionHandler:
         assert result.value == portfolio
         assert reload_record(portfolio).deleted_at is not None
 
-
-class TestPortfolioDeletionValidateHandler:
-    @pytest.fixture
-    def subject(self):
-        return PortfolioDeletionValidateStep
-
-    def test_process_errors_when_portfolio_version_is_activate(
+    def test_handle_portfolio_with_active_portfolio_version(
         self, subject, portfolio, portfolio_version
     ):
         portfolio_version.active = True
         portfolio_version.save()
 
-        result = subject.process(portfolio=portfolio)
+        result = subject.handle(portfolio_id=portfolio.id)
 
         assert result.is_err()
 
-    def test_process_succeeds_when_portfolio_version_is_not_activate(
+    def test_handle_portfolio_with_not_active_portfolio_version(
         self, subject, portfolio, portfolio_version
     ):
         portfolio_version.active = False
         portfolio_version.save()
 
-        result = subject.process(portfolio=portfolio)
+        result = subject.handle(portfolio_id=portfolio.id)
 
         assert result.is_ok()
